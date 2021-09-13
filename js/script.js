580317648ff7519a27e86c25dd604a6d3caa6718
robiulhasanrohan <robiul@blogdesire.com>
2021-09-13 09:39:52 +0600

    finishing experiment successfully

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const music_list = [{
 
 
 // get the audio element
-const audioElement = document.querySelector('audio');
+const audioElement = document.querySelector('#main-audio');
 const playButton = document.querySelector("#play")
 const image = document.getElementById("image")
 const title = document.getElementById("title")
@@ -29,21 +29,30 @@ const nextButton = document.getElementById("next")
 const currenttime = document.getElementById("current-time")
 const duration = document.getElementById("duration")
 const progress = document.getElementById("progress")
+const progresscontainer = document.getElementById("progress-container")
+const repeatButton = document.getElementById("repeat-plist")
 // set some variable
 let isplaying = false
 let songIndex = 1
+let repeateActive = false
 // play song function
 
 
 function currentTime() {
-    let formattedcrntTime = `${Math.floor(audioElement.currentTime / 60)} : ${Math.floor(audioElement.currentTime % 60)} `
-    let formattedDrtn = `${Math.floor(audioElement.duration / 60)} : ${Math.floor(audioElement.duration % 60)} `
-    currenttime.innerText = formattedcrntTime
-    duration.innerText = formattedDrtn
-    progress.style.width = `${audioElement.currentTime / audioElement.duration * 100}%`
+    // avoiding the NaN problem
+    if (audioElement.duration) {
+        let formattedcrntTime = `${Math.floor(audioElement.currentTime / 60)} : ${Math.floor(audioElement.currentTime % 60)} `
+        let formattedDrtn = `${Math.floor(audioElement.duration / 60)} : ${Math.floor(audioElement.duration % 60)} `
+        currenttime.innerText = formattedcrntTime
+        duration.innerText = formattedDrtn
+        progress.style.width = `${audioElement.currentTime / audioElement.duration * 100}%`
+    }
 }
+// adding event to audio  
+audioElement.addEventListener("timeupdate", currentTime)
 
-// setting title,artist,song source etc
+
+// // setting title,artist,song source etc
 function addingSrc(index) {
     audioElement.src = `music/${music_list[index - 1].title}.mp3`
     artist.innerText = music_list[index - 1].artist
@@ -51,16 +60,14 @@ function addingSrc(index) {
     title.innerText = music_list[index - 1].title
 }
 // play song function
-function playSong() {
-    playButton.classList.add("fa-pause")
-    playButton.classList.remove("fa-play")
+async function playSong() {
+    playButton.innerText = "pause"
     audioElement.play()
     isplaying = true
 }
 // pause song function
 function pauseSong() {
-    playButton.classList.remove("fa-pause")
-    playButton.classList.add("fa-play")
+    playButton.innerText = "play_arrow"
     audioElement.pause()
     isplaying = false
 }
@@ -85,24 +92,39 @@ function prevSong() {
 }
 // adding event to next button
 nextButton.addEventListener("click", nextSong)
-// adding event to prev button
+// // adding event to prev button
 prevButton.addEventListener("click", prevSong)
+
+// progress function
+function progres(e) {
+    audioElement.currentTime = e.offsetX / progresscontainer.clientWidth * audioElement.duration
+    progress.style.width = `${(e.offsetX / progresscontainer.clientWidth) * 100}%`
+    playSong()
+}
+
+// progress bar functioanality
+progresscontainer.addEventListener("click", progres)
+
+// repeating song 
+function repeate() {
+    if (repeateActive) {
+        repeatButton.innerText = "repeat"
+        audioElement.loop = false
+        repeateActive = false
+    } else {
+        repeatButton.innerText = "repeat_one"
+        audioElement.loop = true
+        repeateActive = true
+    }
+}
+repeatButton.addEventListener("click", repeate)
+
+
+
 
 //default function after window load
 function windowLoad() {
     addingSrc(songIndex)
-    setInterval(() => {
-        currentTime()
-        // if (audioElement.currentTime = audioElement.duration) nextSong()
-    }, 100);
-
-    // playing next song after finishing the song
-
 }
-progress.addEventListener("click", (e) => {
-    console.log(e.offsetX)
-})
-
 // adding event to window
 window.addEventListener("load", windowLoad())
-
